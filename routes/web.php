23a85dd41277dce78d90cecbd0827e608da4333f
hoangductien312 <hoangductien312@users.noreply.github.com>
2026-03-22 17:42:16 +0700

    fix: Dong bo route trang chu va chi tiet, sua ten bang tours, categories

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,14 @@ use App\Http\Controllers\Admin\TourController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\GuideController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TourUserController;
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 // User - public tour pages
-Route::get('/tours', [\App\Http\Controllers\UserTourController::class, 'index'])->name('user.tours');
-Route::get('/tours/{id}', [\App\Http\Controllers\UserTourController::class, 'show'])->name('user.tour.detail');
+Route::get('/tours', [TourUserController::class, 'index'])->name('tours.index');
+Route::get('/tours/{id}', [TourUserController::class, 'show'])->name('tours.show');
 Route::get('/tours/{id}/booking', [\App\Http\Controllers\BookingController::class, 'create'])->name('user.booking');
 Route::post('/tours/{id}/booking', [\App\Http\Controllers\BookingController::class, 'store'])->name('user.booking.store');
 Route::get('/booking/{bookingId}/success', [\App\Http\Controllers\BookingController::class, 'success'])->name('user.booking.success');
