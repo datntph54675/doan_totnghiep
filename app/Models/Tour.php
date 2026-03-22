bdc704ec94dbf720e1db17a1a02722f2a01ef7de
hoangductien312 <hoangductien312@users.noreply.github.com>
2026-03-22 17:14:23 +0700

    fix bug chi tiết tour

--- app/Models/Tour.php
+++ app/Models/Tour.php
@@ -11,7 +11,7 @@ class Tour extends Model
 {
     use HasFactory;
 
-    protected $table = 'tour';
+    protected $table = 'tours';
     protected $primaryKey = 'tour_id';
     public $timestamps = false;
 
